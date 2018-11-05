dc36e86edf131e6bf741b1fe26c6d24b87c3dccd
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2018-11-05 18:32:37 +0900

    Update DNSClient_lib.c

--- Simple DNS Client/Simple DNS Client/DNSClient_lib.c
+++ Simple DNS Client/Simple DNS Client/DNSClient_lib.c
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 #include "DNSClient_lib.h"
 #pragma warning (disable: 4996)
 
-// ¼ÒÄÏ ÇÔ¼ö ¿À·ù Ãâ·Â ÈÄ Á¾·á
+// ì†Œì¼“ í•¨ìˆ˜ ì˜¤ë¥˜ ì¶œë ¥ í›„ ì¢…ë£Œ
 void err_quit(char *msg)
 {
 	LPVOID lpMsgBuf;
@@ -16,7 +15,7 @@ void err_quit(char *msg)
 	exit(1);
 }
 
-// ¼ÒÄÏ ÇÔ¼ö ¿À·ù Ãâ·Â
+// ì†Œì¼“ í•¨ìˆ˜ ì˜¤ë¥˜ ì¶œë ¥
 void err_display(char *msg)
 {
 	LPVOID lpMsgBuf;
@@ -51,58 +50,4 @@ int RecvHeader(SOCKET sock, DNSDataHeader* recv_len)
 int RecvIP(SOCKET sock, char* buf, short recv_len)
 {
 	return recv(sock, buf, recv_len, 0);
-=======
-#include "DNSClient_lib.h"
-#pragma warning (disable: 4996)
-
-// ¼ÒÄÏ ÇÔ¼ö ¿À·ù Ãâ·Â ÈÄ Á¾·á
-void err_quit(char *msg)
-{
-	LPVOID lpMsgBuf;
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL, WSAGetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf, 0, NULL);
-	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
-	LocalFree(lpMsgBuf);
-	exit(1);
-}
-
-// ¼ÒÄÏ ÇÔ¼ö ¿À·ù Ãâ·Â
-void err_display(char *msg)
-{
-	LPVOID lpMsgBuf;
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL, WSAGetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf, 0, NULL);
-	printf("[%s] %s", msg, (char *)lpMsgBuf);
-	LocalFree(lpMsgBuf);
-}
-void ClientEnd(SOCKET sock)
-{
-	closesocket(sock);
-	WSACleanup();
-}
-
-int SendDomainLength(SOCKET sock, int* send_len)
-{
-	return send(sock, (char *)send_len, sizeof(int), 0);
-}
-
-int SendDomain(SOCKET sock, char* buf, int send_len)
-{
-	return  send(sock, buf, send_len, 0);
-}
-
-int RecvIPLength(SOCKET sock, short* recv_len)
-{
-	return recv(sock, (char*)recv_len, sizeof(short), 0);
-}
-int RecvIP(SOCKET sock, char* buf, short recv_len)
-{
-	return recv(sock, buf, recv_len, 0);
->>>>>>> 59d18402c2039f4eb00bac09463320146b8d21d8
 }
