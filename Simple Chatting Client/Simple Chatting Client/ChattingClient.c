c28016b59892102b33ffc39f75078db1f53cef69
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2018-11-13 17:07:13 +0900

    Add files via upload

---
+++ Simple Chatting Client/Simple Chatting Client/ChattingClient.c
@@ -0,0 +1,73 @@
+#pragma comment(lib, "ws2_32")
+#pragma warning(disable: 4996)
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "ChattingCommon_lib.h"
+extern BOOL is_chatting_over;
+
+int main(int argc, char *argv[])
+{
+	SocketInfo sock_info;
+	HANDLE recv_thread;
+	int retval;
+	// 윈속 초기화
+	WSADATA wsa;
+	if(WSAStartup(MAKEWORD(2,2), &wsa) != 0)
+		return 1;
+
+	ZeroMemory(&sock_info, sizeof(SocketInfo));
+
+	CreateAndConnectSocket(&sock_info);
+
+	recv_thread = CreateThread(NULL, 0, RecvThreadFunction, &sock_info, 0, NULL);
+	printf("If You Want Send Message, Please Enter.(Chatting Over:End)\n");
+
+	while (!is_chatting_over) //1:다 이면 select나 IOCP를 통해서 비동기적 입출력을 수행해야 하지만 1:1이므로 하나의 반복문으로 출력 처리
+	{
+		char temp_buf[BUFSIZE]; //문자열 전환용 임시버퍼
+
+		gets_s(sock_info.send_buf, BUFSIZE);
+		if (is_chatting_over) //채팅이 while문 도중에 읽기 쓰레드에서 끝냈을 경우
+		{
+			printf("Chatting was over!\n");
+			continue;
+		}
+		strncpy(temp_buf, sock_info.send_buf, strnlen(sock_info.send_buf, BUFSIZE-1)+1);
+		strupr(temp_buf);
+
+		if (!strncmp(temp_buf, END_MESSAGE,
+			strnlen(sock_info.send_buf, BUFSIZE))) //종료 메세지 입력확인
+		{
+			retval = SendChttingOverMessage(sock_info.sock);
+
+			if (retval == SOCKET_ERROR)
+			{
+				err_display("Send()");
+				exit(-1);
+			}
+
+			closesocket(sock_info.sock);
+			break;
+		}
+		else
+		{
+			retval = SendChattingMessage(sock_info);
+
+			if (retval == SOCKET_ERROR)
+			{
+
+				if (!is_chatting_over)//채팅이 끝났는데 보낸 걸 수도 있음. 이것을 처리
+				{
+					err_display("Send()");
+					exit(-1);
+				}
+			}
+		}
+	}
+
+	WaitForSingleObject(recv_thread, INFINITE);
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
---
+++ Simple Chatting Client/Simple Chatting Client/ChattingCommon_lib.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef _CHATTING_COMMON_LIB_H__
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define SERVERPORT 9000
+#define BUFSIZE 1024   
+#define SERVERIP   "127.0.0.1"
+#define END_MESSAGE "END" //채팅 종료시 END를 보내면 종료처리
+typedef struct socket_info //쓰레드에 사용하기 위한 소켓 정보 구조체
+{
+	SOCKET sock; //소켓 디스크럽터
+	SOCKADDR_IN sock_addr; //bind()나 connect()로 소켓 주소 정보를 받을 구조체 변수
+	char recv_buf[BUFSIZE]; //수신용 버퍼
+	char send_buf[BUFSIZE]; //송신용 버퍼
+}SocketInfo;
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char *);
+// 소켓 함수 오류 출력
+void err_display(char *);
+// 사용자 정의 데이터 수신 함수
+int recvn(SOCKET, char*, int, int);
+void CreateAndBindSocket(SocketInfo*);//socket()+bind() 기능을 하는 함수
+void CreateAndConnectSocket(SocketInfo*); //socket()+connect() 기능을 하는 함수
+int SendChttingOverMessage(SOCKET); //채팅 종료 메세지 보내는 함수
+int SendChattingMessage(SocketInfo); //일반적인 메세지를 보내는 함수
+DWORD WINAPI RecvThreadFunction(LPVOID);
+#endif
